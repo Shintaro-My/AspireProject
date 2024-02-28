c916c499f8283a7b0faba2e15e44f903f9cfad86
ShintaroMiyahara <s.miyahara@edgeplus.co.jp>
2024-02-28 20:18:23 +0900

    update

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b674380e0614070d4317ff18d5c32fef1e813c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ef64856b648cfc2ddc1822f50dd81f7699faae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/context/sse.tsx
+++ frontend/context/sse.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Dispatch, SetStateAction, createContext, useContext, useState } from "react"
+import { Dispatch, SetStateAction, createContext, useContext, useEffect, useState } from "react"
 
 
 type SSESetter = {
@@ -23,32 +23,45 @@ type Props = {
     children: React.ReactNode
 }
 export const SSEProvider = ({ children, isValid }: Props) => {
+
     const [messages, setMessages] = useState<SSEMessage[]>([])
     const [ping, setPing] = useState<number | null>(null)
 
     const [source, setSource] = useState<EventSource | null>(null)
 
-    if (isValid) {
-        if (!source) {
-            const sse = new EventSource('/sse/default');
-            sse.addEventListener('message', (e: MessageEvent) => {
-                setMessages([JSON.parse(e.data), ...messages].slice(0, 250))
-            })
-            sse.addEventListener('ping', (e: MessageEvent) => {
-                setPing(JSON.parse(e.data))
-            })
-            console.log('SSEProvider: active')
-            setSource(sse)
+    useEffect(() => {
+        if (isValid) {
+            if (!source) {
+                const sse = new EventSource('/sse/default');
+                sse.addEventListener('message', (e: MessageEvent) => {
+                    setMessages([JSON.parse(e.data), ...messages].slice(0, 250))
+                })
+                sse.addEventListener('ping', (e: MessageEvent) => {
+                    setPing(JSON.parse(e.data))
+                })
+                console.log('SSEProvider: active')
+                setSource(sse)
+            }
         }
-    }
-    else {
-        if (source) {
-            source.close()
-            console.log('SSEProvider: inactive')
-            setSource(null)
+        else {
+            if (source) {
+                source.close()
+                console.log('SSEProvider: inactive')
+                setSource(null)
+            }
         }
-    }
+    }, [isValid])
+
+    useEffect(() => {
+        console.log('mount')
+        return () => {
+            console.log('unmount', source)
+            source?.close()
+        }
+    }, [source])
+
     const value: SSESetter = { messages, ping }
+
     return <SSEContext.Provider value={value}>{children}</SSEContext.Provider>
 }
 
